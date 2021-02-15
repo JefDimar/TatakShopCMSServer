b0994dc41d35b2d6a9d0a559bf3333770be5fdb0
jefDimar <jefdimar@gmail.com>
2021-02-15 19:53:09 +0700

    adding table products and authentication middlewares

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,13 +1,11 @@
 const errorHandler = (err, req, res, next) => {
   if(err) {
-    console.log(err.message, err.name, '<<<<<< error di errorhandler');
     switch (err.name) {
       case 'Username / Password wrong':
         res.status(401).json({
           message: 'Invalid email or password, check again'
         })
         break;
-    
       default:
         res.status(500).json({
           message: 'Internal Server Error'
---
+++ models/products.js
@@ -0,0 +1,62 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Products.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a name products'
+        }
+      }
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: {
+          args: true,
+          msg: 'Please enter a valid image url'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a image url'
+        }
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {
+          args: true,
+          msg: 'Please enter a valid integer'
+        }
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {
+          args: true,
+          msg: 'Please enter a valid integer'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Products',
+  });
+  return Products;
+};
