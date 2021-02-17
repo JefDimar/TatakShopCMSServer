1c25f95ea8a47c4585e225862101c8ab3c186c17
jefDimar <jefdimar@gmail.com>
2021-02-17 12:33:09 +0700

    adding errorHandler case from kanban

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,30 @@
 const errorHandler = (err, req, res, next) => {
-  if(err) {
+  if (err) {
     switch (err.name) {
+      case 'SequelizeValidationError':
+        const outputErr = []
+        err.errors.forEach(errors => {
+          outputErr.push(errors.message)
+        })
+        res.status(400).json({
+          message: outputErr
+        })
+        break;
+      case 'SequelizeUniqueConstraintError':
+        res.status(400).json({
+          message: 'Email already registered, try different email'
+        })
+        break;
+      case 'Username / Password wrong':
+        res.status(401).json({
+          message: 'Username or Password incorrect, please login again'
+        })
+        break;
+      case 'JsonWebTokenError':
+        res.status(400).json({
+          message: 'Please provide a JWT token, or login again'
+        })
+        break;
       case 'Username / Password wrong':
         res.status(401).json({
           message: 'Invalid email or password, check again'
@@ -19,6 +43,11 @@ const errorHandler = (err, req, res, next) => {
       case 'Data not found':
         res.status(404).json({
           message: 'Products you requested not found'
+        })
+        break;
+      case 'SequelizeDatabaseError':
+        res.status(400).json({
+          message: 'Database error'
         })
         break;
       default:
