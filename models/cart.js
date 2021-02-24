0b40a3acc41bc080aa3b744b613c5e931dfe0c5a
jefDimar <jefdimar@gmail.com>
2021-02-24 13:35:01 +0700

    adding validations on model cart

--- models/cart.js
+++ models/cart.js
@@ -16,10 +16,58 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Cart.init({
-    productId: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER,
-    quantity: DataTypes.INTEGER,
-    status: DataTypes.BOOLEAN
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a product ID'
+        }
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a user ID'
+        }
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: {
+          args: true,
+          msg: 'Product quantity must be integer'
+        },
+        min: {
+          args: 1,
+          msg: 'Product quantity must be greater than 0'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Please enter a product quantity'
+        }
+      }
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [[true, false]],
+          msg: 'Status must be a boolean value'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'Status must not be empty'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'Cart',
